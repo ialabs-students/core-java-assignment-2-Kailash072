4885f247c542413c3614e2b7804dbc4a6faad641
Kailash072 <106810983+Kailash072@users.noreply.github.com>
2022-06-05 15:50:15 +0530

    Create assigment2.java

---
+++ assigment2.java
@@ -0,0 +1,25 @@
+package assigment2;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class SortArrayList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<Integer>
+        list = new ArrayList<Integer>();
+
+		list.add(24);
+		list.add(543534);
+		list.add(545);
+		list.add(2342);
+		list.add(9);
+
+		System.out.println("Sample Input: "+ list);
+
+	    Collections.sort(list, Collections.reverseOrder());
+
+	    System.out.println("Output: "+ list);
+	}
+}
